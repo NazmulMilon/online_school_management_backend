941b1a07e89225fd911df9f1e675ab42b34b5308
nazmulmilon <milonsarker73@gmail.com>
2023-02-02 15:51:03 +0600

    APP >> users MOD <UserProfile> serializers TeacherListAllSerializer updated

--- online_school_backend/users/serializers.py
+++ online_school_backend/users/serializers.py
@@ -133,17 +133,9 @@ class StudentRetrieveSerializer(ModelSerializer):
 
 
 class TeacherListAllSerializer(ModelSerializer):
-    # first_name = SerializerMethodField()
-    # last_name = SerializerMethodField()
     email = SerializerMethodField()
     fullname = SerializerMethodField()
     username = SerializerMethodField()
-
-    # def get_first_name(self, instance):
-    #     return instance.user.first_name if instance.user else None
-    #
-    # def get_last_name(self, instance):
-    #     return instance.user.last_name if instance.user else None
 
     def get_email(self, instance):
         return instance.user.email if instance.user else None
