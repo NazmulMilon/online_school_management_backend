bf64ff3bb9c04d1368df03da362c3e2b46a3a78d
nazmulmilon <milonsarker73@gmail.com>
2023-02-02 13:49:40 +0600

    APP >> users MOD <UserProfile> urls updated

--- online_school_backend/users/urls.py
+++ online_school_backend/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import UserProfileRetrieveAPIView, UserProfileCreateAPIView, UserProfileListAPIView
+from .views import UserProfileRetrieveAPIView, UserProfileCreateAPIView, UserProfileListAPIView, StudentListAllAPIView
 
 urlpatterns = [
     path('userprofile/all/', UserProfileListAPIView.as_view(), name="all_userprofile"),
     path('userprofile/create/', UserProfileCreateAPIView.as_view(), name="create_userprofile"),
     path('userprofile/retrieve/<int:value>/', UserProfileRetrieveAPIView.as_view(), name="retrieve_userprofile"),
+
+    # search student
+    path('userprofile/all/<str:STUDENT>/', StudentListAllAPIView.as_view(), name="all students"),
 
 ]
