5237b94b1abf34feac847565c3e43bc2bbe1830c
nazmulmilon <milonsarker73@gmail.com>
2023-02-02 15:12:04 +0600

    APP >> users MOD <UserProfile> serializer StudentRetrieveSerializer added

--- online_school_backend/users/serializers.py
+++ online_school_backend/users/serializers.py
@@ -106,3 +106,36 @@ class StudentListAllSerializer(ModelSerializer):
         model = UserProfile
         fields = ["roll", "fullname", "email", "username", "phone_no"]
 
+
+class StudentRetrieveSerializer(ModelSerializer):
+    first_name = SerializerMethodField()
+    last_name = SerializerMethodField()
+    email = SerializerMethodField()
+    fullname = SerializerMethodField()
+    username = SerializerMethodField()
+
+    def get_first_name(self, instance):
+        return instance.user.first_name if instance.user else None
+
+    def get_last_name(self, instance):
+        return instance.user.last_name if instance.user else None
+
+    def get_email(self, instance):
+        return instance.user.email if instance.user else None
+
+    def get_fullname(self, instance):
+        name = ""
+        if instance.user.first_name:
+            name += instance.user.first_name + " "
+        if instance.user.last_name:
+            name += instance.user.last_name
+        return name
+
+    def get_username(self, instance):
+        return instance.user.username if instance.user else None
+
+    class Meta:
+        model = UserProfile
+        fields = ["roll", "first_name", "last_name", "fullname", "email", "username", "father_name", "mother_name",
+                  "date_of_birth", "phone_no", "user_role", "address"]
+
