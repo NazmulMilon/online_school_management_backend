3f85e2df5085fdbe3946fe5a09c910c9cfab1d4f
nazmulmilon <milonsarker73@gmail.com>
2023-02-06 16:34:21 +0600

    APP >> users MOD <UserProfile> serializers updated

--- online_school_backend/users/serializers.py
+++ online_school_backend/users/serializers.py
@@ -130,4 +130,126 @@ class ParentListSerializer(ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ["fullname", "email", "username", "phone_no", "user_role"]
+        fields = ["id", "fullname", "email", "username", "phone_no", "user_role"]
+
+
+class StudentSerializer(ModelSerializer):
+    # parent = SerializerMethodField()
+    #
+    # def get_parent(self, instance):
+    #     parent_obj = UserProfile.objects.filter(id=instance.parent_id).first()
+    #     return StudentRetrieveSerializer(parent_obj).data
+
+    class Meta:
+        model = UserProfile
+        exclude = ['created_at', 'updated_at', 'parent']
+
+
+class ParentRetrieveSerializer(ModelSerializer):
+    first_name = SerializerMethodField()
+    last_name = SerializerMethodField()
+    email = SerializerMethodField()
+    fullname = SerializerMethodField()
+    username = SerializerMethodField()
+
+    parent = SerializerMethodField()
+
+    def get_first_name(self, instance):
+        return instance.user.first_name if instance.user else None
+
+    def get_last_name(self, instance):
+        return instance.user.last_name if instance.user else None
+
+    def get_email(self, instance):
+        return instance.user.email if instance.user else None
+
+    def get_fullname(self, instance):
+        name = ""
+        if instance.user.first_name:
+            name += instance.user.first_name + " "
+        if instance.user.last_name:
+            name += instance.user.last_name
+        return name
+
+    def get_username(self, instance):
+        return instance.user.username if instance.user else None
+
+    def get_parent(self, instance):
+        parent_obj = UserProfile.objects.filter(id=instance.parent_id).first()
+        return StudentSerializer(parent_obj).data
+
+    class Meta:
+        model = UserProfile
+        # fields = ["roll", "first_name", "last_name", "fullname", "email", "username", "father_name", "mother_name",
+        #           "date_of_birth", "phone_no", "user_role", "address"]
+        exclude = ['created_at', 'updated_at']
+
+
+class StudentListAllSerializer(ModelSerializer):
+    email = SerializerMethodField()
+    fullname = SerializerMethodField()
+    username = SerializerMethodField()
+
+    def get_email(self, instance):
+        return instance.user.email if instance.user else None
+
+    def get_fullname(self, instance):
+        name = ""
+        if instance.user.first_name:
+            name += instance.user.first_name + " "
+        if instance.user.last_name:
+            name += instance.user.last_name
+        return name
+
+    def get_username(self, instance):
+        return instance.user.username if instance.user else None
+
+    class Meta:
+        model = UserProfile
+        fields = ["id", "roll", "fullname", "email", "username", "phone_no"]
+
+
+class ParentSerializer(ModelSerializer):
+    class Meta:
+        model = UserProfile
+        exclude = ['created_at', 'updated_at', 'parent']
+
+
+class StudentRetrieveSerializer(ModelSerializer):
+    first_name = SerializerMethodField()
+    last_name = SerializerMethodField()
+    email = SerializerMethodField()
+    fullname = SerializerMethodField()
+    username = SerializerMethodField()
+    parent = SerializerMethodField()
+
+    def get_first_name(self, instance):
+        return instance.user.first_name if instance.user else None
+
+    def get_last_name(self, instance):
+        return instance.user.last_name if instance.user else None
+
+    def get_email(self, instance):
+        return instance.user.email if instance.user else None
+
+    def get_fullname(self, instance):
+        name = ""
+        if instance.user.first_name:
+            name += instance.user.first_name + " "
+        if instance.user.last_name:
+            name += instance.user.last_name
+        return name
+
+    def get_username(self, instance):
+        return instance.user.username if instance.user else None
+
+    def get_parent(self, instance):
+        parent_obj = UserProfile.objects.filter(parent=instance).first()
+        return ParentSerializer(parent_obj).data
+
+    class Meta:
+        model = UserProfile
+        # fields = ["roll", "first_name", "last_name", "fullname", "email", "username", "father_name", "mother_name",
+        #           "date_of_birth", "phone_no", "user_role", "address", "parent"]
+
+        exclude = ['created_at', 'updated_at']
