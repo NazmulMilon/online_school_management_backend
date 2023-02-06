8e4c74be3d29cba3a8ebfbb6847266b51878a6c5
nazmulmilon <milonsarker73@gmail.com>
2023-02-06 15:26:03 +0600

    APP >> users MOD <UserProfile> fields updated

--- online_school_backend/users/models.py
+++ online_school_backend/users/models.py
@@ -8,13 +8,14 @@ from systems.enums import UserType
 
 class UserProfile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, help_text="user")
-    father_name = models.CharField(max_length=100, help_text="user's father name")
-    mother_name = models.CharField(max_length=100, help_text="user's mother name ")
-    date_of_birth = models.DateField(auto_now_add=True, help_text="user's date of birth")
+    father_name = models.CharField(max_length=100, help_text="user's father name", blank=True, null=True)
+    mother_name = models.CharField(max_length=100, help_text="user's mother name ", blank=True, null=True)
+    date_of_birth = models.DateField(auto_now_add=False, help_text="user's date of birth")
     phone_no = models.IntegerField(help_text="user's phone number")
     user_role = models.CharField(max_length=10, choices=UserType.choices(), default=UserType.STUDENT_TYPE.value)
     address = models.CharField(max_length=100, help_text="user's address")
     roll = models.IntegerField(null=True, blank=True, help_text="student's roll number")
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
         db_table = 'user_profiles'
