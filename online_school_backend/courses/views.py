40c55b41ac7390d836c0eff7e03ecb4cf4c579e0
nazmulmilon <milonsarker73@gmail.com>
2023-02-07 16:33:47 +0600

    APP >> courses MOD <Enrolment> views added

--- online_school_backend/courses/views.py
+++ online_school_backend/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Course
-from .serializers import CourseCreateSerializer, CourseRetrieveSerializer, CourseListSerializer
+from .models import Course, Enrolment
+from .serializers import CourseCreateSerializer, CourseRetrieveSerializer, CourseListSerializer, EnrolmentListSerializer, \
+    UserRetrieveSerializer, EnrolmentSerializer
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,4 +14,28 @@ class CourseListAPIView(ListAPIView):
     def get(self, request, *args, **kwargs):
         queryset = Course.objects.all()
         serializer = CourseListSerializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class EnrolmentListAPIView(ListAPIView):
+    serializer_class = EnrolmentListSerializer
+    queryset = Enrolment.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        data = dict()
+        queryset = Enrolment.objects.filter(user_id=pk).first()
+
+        # course_list = []
+        # for item in queryset:
+        #     course_data = dict()
+        #     course_data["id"] = item.course.id
+        #     course_data["course_code"] = item.course.course_code
+        #     course_data["course_name"] = item.course.course_name
+        #     course_list.append(course_data)
+        #
+        # data["user"] = pk
+        # data["course"] = course_list
+        serializer = EnrolmentListSerializer(queryset, many=False)
+        # return Response(data=data, status=status.HTTP_200_OK)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
