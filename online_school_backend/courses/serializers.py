562f9fd5a0c3591fae1c88d11d55915e32644090
nazmulmilon <milonsarker73@gmail.com>
2023-02-07 16:32:49 +0600

    APP >> courses MOD <Course, Enrolment> serializers created

--- online_school_backend/courses/serializers.py
+++ online_school_backend/courses/serializers.py
@@ -19,3 +19,48 @@ class CourseRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Course
         exclude = ['created_at', 'updated_at']
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class EnrolmentSerializer(ModelSerializer):
+    class Meta:
+        model = Enrolment
+        exclude = ['created_at', 'updated_at']
+
+
+class EnrolmentListSerializer(ModelSerializer):
+    user = SerializerMethodField()
+    course = SerializerMethodField()
+
+    def get_course(self, instance):
+        course_id_list = []
+        enrol_qs = Enrolment.objects.filter(user=instance.user).values("course")
+        for item in enrol_qs:
+            if item["course"] and item["course"] not in course_id_list:
+                course_id_list.append(item["course"])
+        course_queryset = Course.objects.filter(pk__in=course_id_list)
+        return CourseListSerializer(course_queryset, many=True).data
+
+    def get_user(self, instance):
+        return instance.user.id
+
+    class Meta:
+        model = Enrolment
+        exclude = ['id', 'created_at', 'updated_at']
+
+
+class UserRetrieveSerializer(ModelSerializer):
+    course = SerializerMethodField()
+
+    def get_course(self, instance):
+        queryset = Course.objects.filter(id=instance.course_id)
+        return CourseListSerializer(queryset, many=True).data
+
+    class Meta:
+        model = Enrolment
+        exclude = ['created_at', 'updated_at']
