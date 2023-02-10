7480d0e0ff22cad3bbf0c2d821161871b4bec745
nazmulmilon <milonsarker73@gmail.com>
2023-02-10 14:39:21 +0600

    APP >> courses MOD <Attendance> admin file updated

--- online_school_backend/courses/admin.py
+++ online_school_backend/courses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Course, Enrolment
+from .models import Course, Enrolment, Attendance
 
 admin.site.register(Course)
 admin.site.register(Enrolment)
+admin.site.register(Attendance)
