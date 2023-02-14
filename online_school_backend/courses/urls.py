3a7ea4d668ac8c01b196185237a3ab2c9e3b71aa
nazmulmilon <milonsarker73@gmail.com>
2023-02-14 16:24:50 +0600

    urls updated

--- online_school_backend/courses/urls.py
+++ online_school_backend/courses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CourseListAPIView, EnrollmentListAPIView, EnrollmentCreateAPIView, EnrolmentRetrieveAPIView, \
-    EnrolmentRetrieveByCourseAPIView
+    EnrolmentRetrieveByCourseAPIView, AttendanceListAPIView, AttendanceRetrieveAPIView, AttendanceCreateAPIView
 
 urlpatterns = [
     path('course/all/', CourseListAPIView.as_view(), name='all_course'),
