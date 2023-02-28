933e0c02ee08f755fd6387e24f05e9e80f10604e
nazmulmilon <milonsarker73@gmail.com>
2023-02-28 10:23:41 +0600

    settings file updated

--- online_school_backend/online_school_backend/settings.py
+++ online_school_backend/online_school_backend/settings.py
@@ -136,3 +136,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'rest_framework.permissions.IsAdminUser'
 #    ),
 # }
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
