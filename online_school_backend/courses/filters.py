97c58552ff5628c803456c328a34bb586d2c4ded
nazmulmilon <milonsarker73@gmail.com>
2023-02-28 10:21:59 +0600

    app >> courses MOD <Courses> filters.py file updated

--- online_school_backend/courses/filters.py
+++ online_school_backend/courses/filters.py
@@ -1,10 +1,13 @@
 import django_filters
 from .models import Course
+from django_filters import rest_framework as filters
 
 
-class CourseFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='iexact')
+class CourseFilter(filters.FilterSet):
+    course_name = filters.CharFilter(field_name='course_name', lookup_expr='icontains')
+    # course_name = filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Course
+        # fields = "__all__"
         fields = ['course_code', 'course_name']
